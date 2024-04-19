b4290021ceeab2f41b1d9960efbc11940e25d264
谭谭 <100819988+QiPanTanYi@users.noreply.github.com>
2024-04-19 18:18:25 +0800

    add it
    add it

---
+++ links.js
@@ -0,0 +1,159 @@
+  // {
+  //   text: "",
+  //   url: "",
+  // },
+
+
+var links = [
+  {
+      text: "ChatGPT(免费)",
+      url: "https://chat18.aichatos.xyz/",
+  },
+  {
+      text: "Kimi.ai",
+      url: "https://kimi.moonshot.cn/",
+  },
+  {
+    text: "有道翻译",
+    url: "https://fanyi.youdao.com/",
+  },
+  {
+    text: "icon生成器",
+    url: "https://favicon.io/favicon-converter/#google_vignette",
+  },
+  {
+    text: "163邮箱",
+    url: "https://email.163.com/",
+  },
+  {
+      text: "阿里云服务器",
+      url: "https://swasnext.console.aliyun.com/",
+  },
+  {
+    text: "云短信服务",
+    url: "https://dysms.console.aliyun.com/overview",
+  },
+  {
+    text: "Qwerty Learner(练习打字)",
+    url: "https://qwerty.kaiyi.cool/",
+  },
+    {
+    text: "MinIO 文档",
+    url: "https://www.minio.org.cn/download.shtml#/linux",
+  },
+  {
+    text: "Flutter 文档",
+    url: "https://flutter.cn/docs",
+  },
+  {
+    text: "C语言",
+    url: "https://www.runoob.com/cprogramming/c-data-types.html",
+  },
+  {
+    text: "uni-app文档",
+    url: "https://uniapp.dcloud.net.cn/component/",
+  },
+  {
+    text: "ECharts文档",
+    url: "https://echarts.apache.org/zh/index.html",
+  },
+  {
+    text: "数仓笔记",
+    url: "https://ykg8hl7h33.feishu.cn/docx/I1U3dptypou74wxPleicl3YMn5c",
+  },
+  {
+    text: "林子雨大数据课程",
+    url: "https://dblab.xmu.edu.cn/blog/",
+  },
+  {
+    text: "菜鸟教程",
+    url: "https://www.runoob.com/html/html-tutorial.html",
+  },
+  {
+    text: "Vue3 文档",
+    url: "https://cn.vuejs.org/guide/introduction.html",
+  },
+  {
+    text: "Node.js文档",
+    url: "http://www.nodejs.com.cn/",
+  },
+  {
+    text: "React文档",
+    url: "https://www.ltonus.com/react/docs/getting-started.html",
+  },
+  {
+    text: "Sass文档",
+    url: "https://www.sass.hk/docs/",
+  },
+  {
+    text: "Element文档",
+    url: "https://element-plus.org/zh-CN/#/zh-CN",
+  },
+  {
+    text: "iconfont 矢量图标",
+    url: "https://www.iconfont.cn/",
+  },
+  {
+    text: "sklearn中文文档",
+    url: "https://sklearn.apachecn.org/#/",
+  },
+{
+  text: "字节内部-Go圣经",
+  url: "https://www.bookstack.cn/read/go42/SUMMARY.md",
+},  
+  {
+      text: "免费API接口",
+      url: "https://api.aa1.cn/",
+  },
+  {
+      text: "B站",
+      url: "https://www.bilibili.com/",
+  },
+  {
+      text: "知乎",
+      url: "https://www.zhihu.com/",
+  },
+  {
+      text: "阿里云盘（网页版）",
+      url: "https://www.aliyundrive.com/drive",
+  },
+  {
+      text: "腾讯云服务器",
+      url: "https://cloud.tencent.com/login?s_url=https%3A%2F%2Fconsole.cloud.tencent.com%2Flighthouse%2Finstance%2Findex",
+  },
+
+  {
+      text: "力扣",
+      url: "https://leetcode-cn.com/leetbook/",
+  },
+  {
+      text: "PTA",
+      url: "https://pintia.cn/problem-sets/994805342720868352/problems/994805528788582400",
+  },
+
+  {
+      text: "CSDN社区",
+      url: "https://www.csdn.net/c/?spm=1001.2101.3001.6068",
+  },
+  {
+      text: "掘金社区",
+      url: "https://juejin.cn/",
+  },
+  {
+      text: "Gitee码云",
+      url: "https://gitee.com/dashboard",
+  },
+  {
+    text: "Kaggle",
+    url: "https://www.kaggle.com/",
+  },
+  {
+      text: "GitHub",
+      url: "https://github.com/",
+  },
+  {
+      text: "ChatGPT(官网,国内无法访问)",
+      url: "https://chat.openai.com/chat",
+  }
+
+];
